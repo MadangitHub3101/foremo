c9e76c50be0bd003e953d229ff6571bd960fd2d7
MadangitHub3101 <madanadattaburra@gmail.com>
2026-03-25 18:57:02 +0530

    updated 2st

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const PORT = 5000;
 // Simple API route
 app.get('/api/health', (req, res) => {
   res.json({
-    message: "Thank you Madanadat, backend is working :)"
+    message: "Thank you Madanadatta, backend is working :)"
   });
 });
 
