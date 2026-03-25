08564247531bea2521d3262e7053850b2a2d16f9
MadangitHub3101 <madanadattaburra@gmail.com>
2026-03-25 12:46:26 +0530

    updated backend response mesaage

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const PORT = 5000;
 // Simple API route
 app.get('/api', (req, res) => {
   res.json({
-    message: "Thank you Madan, backend is working :)"
+    message: "Thank you Madana, backend is working :)"
   });
 });
 
